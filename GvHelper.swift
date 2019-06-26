0fc452bc0b068a1090cb05dde89666a841444d29
prince@SW4 <prince.geovision.tw@gmail.com>
2019-06-26 15:19:36 +0800

    add version to helper.

--- GvHelper.swift
+++ GvHelper.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class GvHelper: NSObject {
+    
+    private let version_major : Int = 0
+    private let version_minor : Int = 1
+    private let version_patch : Int = 0
+    
+    private override init(){}
+    public static let hp: GvHelper = {
+        let instance = GvHelper()
+        return instance
+    }()
+    public var version: String {
+        get{
+            return "\(version_major).\(version_minor).\(version_patch)"
+        }
+    }
+}
