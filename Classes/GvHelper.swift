6819d0afddfc2d7e9a7daae52cad045c5bc8bd9e
prince <princejar@gmail.com>
2019-07-17 18:20:15 +0800

    update source code.

--- Classes/GvHelper.swift
+++ Classes/GvHelper.swift
@@ -12,7 +12,7 @@ public class GvHelper: NSObject {
     
     private let version_major : Int = 0
     private let version_minor : Int = 2
-    private let version_patch : Int = 1
+    private let version_patch : Int = 2
     
     private override init(){}
     public static let hp: GvHelper = {
--- README.md
+++ README.md
@@ -16,6 +16,7 @@ gvhelper is available under the MIT license. See the LICENSE file for more info.
 
 
 ## Change Logs:
+- 0.2.2: Add source code path.
 - 0.2.1: Update iOS Support Version to 10.3
 - 0.2.0: Add VersionVC2
 - 0.1.1: Add GEO Data Model for internal use.
